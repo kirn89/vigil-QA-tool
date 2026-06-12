91524ded5dda101287acc6fcd0235e7e0fd0dd47
kirn89 <kiran.rai8991@gmail.com>
2026-06-12 11:25:22 +0100

    chore: remove test files

--- packages/engine/test-cli2.mjs
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath } from 'node:url';
-
-console.log('process.argv:', process.argv);
-console.log('process.argv.slice(2):', process.argv.slice(2));
-
-if (process.argv[1] === fileURLToPath(import.meta.url)) {
-  console.log('Running as script');
-}
